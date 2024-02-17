99db4e5f5157d0a7d585e17b3b981b13aac1266c
Tejas Paradkar <67867777+tejasparadkar@users.noreply.github.com>
2024-02-17 11:23:32 +0530

    Add files via upload

--- P1.java
+++ P1.java
@@ -1,7 +1,43 @@
-class P1//print hello world
+class  P1//static variables,methods,blocks tracing
 {
+	static char a;
+	static boolean b;
+	public static void Qspiders()
+	{
+		System.out.println("QSP begins");
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		a='a';
+		b=true;
+		Jspiders();
+		System.out.println("QSP ends");
+	}
+	public static void Jspiders()
+	{
+		System.out.println("JSP begins");
+		System.out.println("a:" +a);
+		System.out.println("b: "+b);
+		a='b';
+		b=false;
+		System.out.println("JSP ends");
+	}
+	static
+	{
+		System.out.println("SIB-1");
+	}
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("Main begins");
+		char a='a';
+		Qspiders();
+		System.out.println("a: "+a);
+		System.out.println("a: "+P1.a);
+		System.out.println("b: "+b);
+		System.out.println("Main ends");
+	}
+	static
+	{
+		System.out.println("SIB-2");
+		Jspiders();
 	}
 }
