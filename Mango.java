512dd647d39e0922dc6391522a87ece5e5132026
Tejas Paradkar <67867777+tejasparadkar@users.noreply.github.com>
2024-03-19 12:01:07 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,15 @@
+package object_class;
+
+class Monkey {
+	
+}
+public class Mango
+{
+public static void main(String[] args) {
+	Monkey ref=new Monkey();
+	System.out.println(ref);
+	System.out.println(ref.toString());
+	
+	
+}
+}
---
+++ Mango1.java
@@ -0,0 +1,18 @@
+package object_class;
+
+class Monkey1 {
+	public String toString()
+	{
+		return "ma'am cute";
+	}
+}
+public class Mango1
+{
+public static void main(String[] args) {
+	Monkey1 ref=new Monkey1();
+	System.out.println(ref);
+	System.out.println(ref.toString());
+	
+	
+}
+}
