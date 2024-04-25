4d26ac0223a5109b569b11ff472332b862ef8c0c
Tejas Paradkar <67867777+tejasparadkar@users.noreply.github.com>
2024-04-25 22:06:05 +0530

    Add files via upload

---
+++ Comparator2.java
@@ -0,0 +1,107 @@
+package collectionProgram;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+class Pupil
+{
+	int rollno;
+	String name;
+	double marks;
+	int age;
+	public Pupil(int rollno, String name, double marks, int age) {
+		this.rollno = rollno;
+		this.name = name;
+		this.marks = marks;
+		this.age = age;
+	}
+	public int compareTo(Student o)
+	{
+		if(this.marks>o.marks)
+		{
+			return 1;
+		}
+		else if(this.marks<o.marks)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	@Override
+	public String toString() {
+		return rollno + " "+ name +" "+marks+" "+age;
+	}
+}
+class MarksCompare implements Comparator<Pupil>
+{
+	@Override
+	public int compare(Pupil o1, Pupil o2) {
+		return (int) (o1.marks-o2.marks);
+	}
+}
+class AgeCompare implements Comparator<Pupil>
+{
+	@Override
+	public int compare(Pupil o1, Pupil o2) {
+		return (o1.age-o2.age);
+	}
+}
+
+public class Comparator2{
+
+	public static void main(String[] args) {
+		ArrayList<Pupil> a =new ArrayList<Pupil>();
+		a.add(new Pupil(1, "A", 71, 23));
+		a.add(new Pupil(2, "B", 84, 26));
+		a.add(new Pupil(3, "C", 84, 24));
+		a.add(new Pupil(4, "D", 76, 25));
+		MarksCompare m = new MarksCompare();
+		Collections.sort(a,m);
+		ArrayList<Double> marks = new ArrayList<>();
+		ArrayList<Double> rmarks = new ArrayList<>();
+		ArrayList<Pupil> repeatedPupils =new ArrayList<Pupil>();
+		for(Pupil s:a)
+		{
+			if(marks.contains(s.marks)) {
+				repeatedPupils.add(s);
+				rmarks.add(s.marks);
+			}
+			marks.add(s.marks);
+			System.out.println(s);
+		}
+		
+	  final HashSet<Double> ss  = new HashSet<Double>(rmarks);
+	  for(Double s:ss) 
+		{
+		  for(Pupil p:a) {
+			  if(p.marks == s) {
+				  repeatedPupils.add(p);
+				  break;
+			  }
+		  }
+			
+		}
+		
+		System.out.println("---------------" );
+//		AgeCompare age = new AgeCompare(); 
+//		Collections.sort(a,age.reversed()); 
+//		for(Pupil s:a) 
+//		{
+//			System.out.println(s); 
+//		}
+//		System.out.println("---------------");
+		
+		AgeCompare age = new AgeCompare(); 
+		Collections.sort(repeatedPupils,age.reversed());
+		for(Pupil s:repeatedPupils) 
+		{
+			System.out.println(s); 
+		}
+	}
+
+}
