4129e218ecf8bd8605b92603dac5d53182b4685f
Tejas Paradkar <67867777+tejasparadkar@users.noreply.github.com>
2024-06-10 09:46:20 +0530

    Update StringIntToRoman.java

--- StringIntToRoman.java
+++ StringIntToRoman.java
@@ -1,62 +1,106 @@
 package string;
-
-import java.util.HashMap;
-import java.util.Map;
-
 //WAJP to convert Int to Roman
 public class StringIntToRoman {
-
+	public static String intToRoman(int num) {
+        String roman="";
+        while(num!=0)
+        {
+            if(num>=1000)
+            {
+                roman=roman+"M";
+                num=num-1000;
+            }
+            else if(num<1000 && num>=500)
+            {
+                if(num>=900&&num<1000)
+                {
+                    roman=roman+"CM";
+                    num=num-900;
+                    
+                }
+                else 
+                {
+                    roman=roman+"D";
+                    num=num-500;
+                }
+            }
+            else if(num<500 && num>=100)
+            {
+                if(num>=400&&num<500)
+                {
+                    roman=roman+"CD";
+                    num=num-400;
+                    
+                }
+                else 
+                {
+                    roman=roman+"C";
+                    num=num-100;
+                }
+            }
+            else if(num<100 && num>=50)
+            {
+                if(num>=90&&num<100)
+                {
+                    roman=roman+"XC";
+                    num=num-90;
+                    
+                }
+                else 
+                {
+                    roman=roman+"L";
+                    num=num-50;
+                }
+            }
+            else if(num<50 && num>=10)
+            {
+                if(num>=40&&num<50)
+                {
+                    roman=roman+"XL";
+                    num=num-40;
+                    
+                }
+                else 
+                {
+                    roman=roman+"X";
+                    num=num-10;
+                }
+            }
+            else if(num<10 && num>=5)
+            {
+                if(num>=9&&num<10)
+                {
+                    roman=roman+"IX";
+                    num=num-9;
+                   
+                }
+                else 
+                {
+                    roman=roman+"V";
+                    num=num-5;
+                }
+            }
+            else if(num<5 && num>=1)
+            {
+                if(num>=4&&num<5)
+                {
+                    roman=roman+"IV";
+                    num=num-4;
+                 
+                }
+                else 
+                {
+                    roman=roman+"I";
+                    num=num-1;
+                }
+            }
+            
+        }
+        return roman;
+    }
 	public static void main(String[] args) {
-		int n = 4949;
-		Map<Integer, Character> roman = new HashMap<Integer, Character>();
-		roman.put(1, 'I');
-		roman.put(5, 'V');
-		roman.put(10,'X');
-		roman.put(50,'L');
-		roman.put(100,'C');
-		roman.put(500,'D');
-		roman.put(1000,'M');
-		String s="";
-		while(n!=0)
-		{
-			if(n>=1000)
-			{
-				s=s+"M";
-				n=n-1000;
-			}
-			else if(n>=500 && n<1000)
-			{
-				s=s+"D";
-				n=n-500;
-			}
-			else if(n>=100 && n<500)
-			{
-				s=s+"C";
-				n=n-100;
-			}
-			else if(n>=50 && n<100)
-			{
-				s=s+"L";
-				n=n-50;
-			}
-			else if(n>=10 && n<50)
-			{
-				s=s+"X";
-				n=n-10;
-			}
-			else
-			{
-				s=s+"I";
-				n=n-1;
-			}
-		}
-		s = s.replace("IIII","IV");
-		s = s.replace("VIIII","IX");
-		s = s.replace("XXXX","XL");
-		s = s.replace("LXXXX","XC");
-		s = s.replace("CCCC","CD");
-		s = s.replace("DCCCC","CM");
-		System.out.println(s);
+		int n = 3749;
+		System.out.println(intToRoman(n));
 		
 	}
 
